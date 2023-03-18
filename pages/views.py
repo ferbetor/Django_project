97395dfc31d8a2745a0cb34120296e23586af2c1
ferbetor <116447207+ferbetor@users.noreply.github.com>
2023-03-18 17:10:58 -0400

    first commit

---
+++ pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+# generic views for work with templates
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "home.html"
